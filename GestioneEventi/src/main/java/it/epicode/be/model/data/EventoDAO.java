d4f0087dab0ef3155a35dab3f557d6da60b29149
LucaB <LucaB@DESKTOP-9ILK5NC.mshome.net>
2021-07-19 18:20:27 +0200

    update 1

--- GestioneEventi/src/main/java/it/epicode/be/model/data/EventoDAO.java
+++ GestioneEventi/src/main/java/it/epicode/be/model/data/EventoDAO.java
@@ -1,22 +1,75 @@
 package it.epicode.be.model.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import it.epicode.be.model.Evento;
 
 public class EventoDAO {
 
 	public void save(Evento ev) {
-		//TODO
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("GestioneEventi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(ev);
+			em.getTransaction().commit();
+
+		} finally {
+			em.close();
+			factory.close();
+		}
 	}
-	public Evento getById() {
-		//TODO
-		return null;
+
+	public Evento getById(Long id) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("GestioneEventi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			Evento found = em.find(Evento.class, id);
+			return found;
+		} finally {
+			em.close();
+			factory.close();
+		}
+
 	}
-	public void delete() {
-		//TODO
+
+	public void delete(Long id) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("GestioneEventi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			//em.persist(ev);
+			em.getTransaction().commit();
+			//TODO
+		} finally {
+			em.close();
+			factory.close();
+		}
 	}
-	public void refresh() {
-		//TODO
+
+	public void refresh(Evento ev) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("GestioneEventi");
+		EntityManager em = factory.createEntityManager();
+
+		try {
+
+			Evento updated = em.find(Evento.class, ev.getId());
+			updated.setTitolo(ev.getTitolo());
+			updated.setDataEvento(ev.getDataEvento());
+			updated.setDescrizione(ev.getDescrizione());
+			updated.setTipoEvento(ev.getTipoEvento());
+			updated.setNumeroMassimoPartecipanti(ev.getNumeroMassimoPartecipanti());
+
+			em.getTransaction().begin();
+			em.persist(updated);
+			em.getTransaction().commit();
+
+		} finally {
+			em.close();
+			factory.close();
+		}
 	}
-	
-	
+
 }
